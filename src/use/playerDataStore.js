b49e79c54d5a4d065fca9037fe44cc8c82fd04e3
AstridV0522 <108079929+AstridV0522@users.noreply.github.com>
2022-07-28 14:59:14 -0500

    we worked on the history array

--- src/use/playerDataStore.js
+++ src/use/playerDataStore.js
@@ -2,13 +2,55 @@ import { reactive } from 'vue'
 
 export const playerDataStore = reactive({
     overconfidenceScore:0,
-    didGetAdvisor:false,
+    //didGetAdvisor:false,
+    isAdvisorEnabled: false,
     timeSpentInPause:0,
     timeSpentInSimulation:0,
     articlesRead:[],
-    tradeHistory:{},
-    currentPortfolio:{},
+    // type, price, was the game pause or not, day, number of share, total value, percent invested 
+    tradeHistory:[],
+    currentPortfolio:{
+        "CROC":{
+            numberOfShares: 0,
+            totalValue: 0,
+            percentageValue: 0,
+            recommendedPercentage: 0,
+            isInPortfolio: false,    
+        },
+        "SLTH":{
+            numberOfShares: 0,
+            totalValue: 0,
+            percentageValue: 0,
+            recommendedPercentage: 0,
+            isInPortfolio: false,
+        },
+        "TURT":{
+            numberOfShares: 0,
+            totalValue: 0,
+            percentageValue: 0,
+            recommendedPercentage: 0,
+            isInPortfolio: false, 
+        },
+        "GIRA":{
+            numberOfShares: 0,
+            totalValue: 0,
+            percentageValue: 0,
+            recommendedPercentage: 0,
+            isInPortfolio: false,    
+        },
+        "BUNY":{
+            numberOfShares: 0,
+            totalValue: 0,
+            percentageValue: 0,
+            recommendedPercentage: 0,
+            isInPortfolio: false, 
+        }
+    },
 
+
+    get numArticlesRead(){
+        return this.articlesRead.length
+    },
 
     markArticleAsRead(){
         //SOMEHOW STORE THIS ARTICLE AS READ
@@ -16,9 +58,30 @@ export const playerDataStore = reactive({
         console.log("article was read")
     },
 
-    get numArticlesRead(){
-        return this.articlesRead.length
-    },
+    addStock(ticker, numberShares,SharePrice,currentDay){
+		//this.purchasedStockName.push(sock)
+		portfolio[ticker]['numberOfShares']+=numberShares
+		portfolio[ticker]['totalValue']+= numberShares*pricePerShare
+		portfolio[ticker]['isInPortfolio']=true
+		history = {
+            ticker:ticker,
+			day: currentDay,
+            tradeType:"buy",
+			numberOfShares: numberShares, 
+            SharePrice:SharePrice,
+			totalValue: numberShares*SharePrice,
+			percentageOfInvestedMoney: totalValue/currentMoney * 100,
+            isTimeRunning:boolean, // fix this later
+		}
+		portfolio[ticker]['tradeHistory'].push(history)
+		currentMoney-= numberShares*pricePerShare
+
+		if (this.holding['percentageInvested']>= 50 ){
+			this.overConfidence+= 10
+		}
+	},
+
+ 
 
     incrementOverConfidenceScore(value){
         this.overconfidenceScore+=value
