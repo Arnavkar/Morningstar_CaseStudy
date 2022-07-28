1c13a923281c94cea0c2fb5c006993d9e7f4fd55
AstridV0522 <108079929+AstridV0522@users.noreply.github.com>
2022-07-28 12:55:58 -0500

    We are only adding the work we have done in playerTracker nothing else has been touch

--- playerTracker.js
+++ playerTracker.js
@@ -4,63 +4,110 @@ class PlayerDataTracker{
 	constructor(initialMoney,SIMULATED_DURATION_IN_DAYS,REAL_DURATION_IN_MINUTES,article_data){
 		this.isAdvisorEnabled = false
 		this.pauseCount = 0
+		this.overConfidence = 0 // we add points when they are being too coky 
+		this.currentMoney = 0
 		this.articleClickCount = 0
 		this.investedMoney = 0
 		this.uninvestedMoney = initialMoney
 		this.timeUtility = new TimeUtility(SIMULATED_DURATION_IN_DAYS,REAL_DURATION_IN_MINUTES,article_data)
-		this.portfolio = {}
-	}
-	
-	addStockToPurchasedStock(stock){
-		this.purchasedStockName.push(stock)
+		this.portfolio = {
+			"CROC":{
+				numberOfShares: 0,
+				totalValue: 0,
+				percentageValue: 0,
+				recommendedPercentage: 0,
+				isInPortfolio: false, 
+				tradeHistory:[]
+			},
+			"SLTH":{
+				numberOfShares: 0,
+				totalValue: 0,
+				percentageValue: 0,
+				recommendedPercentage: 0,
+				isInPortfolio: false, 
+				tradeHistory:[]
+			},
+			"TURT":{
+				numberOfShares: 0,
+				totalValue: 0,
+				percentageValue: 0,
+				recommendedPercentage: 0,
+				isInPortfolio: false, 
+				tradeHistory:[]
+			},
+			"GIRA":{
+				numberOfShares: 0,
+				totalValue: 0,
+				percentageValue: 0,
+				recommendedPercentage: 0,
+				isInPortfolio: false, 
+				tradeHistory:[]
+			},
+			"BUNY":{
+				numberOfShares: 0,
+				totalValue: 0,
+				percentageValue: 0,
+				recommendedPercentage: 0,
+				isInPortfolio: false, 
+				tradeHistory:[]
+			}
+		}
+	}	
+	// for addStock w are updating numberOfShare,totoalValue, isInProtfolio, 
+	//add stuff to current holding and trandHistory
+
+	addStock(ticker, numberShares, pricePerShare,currentDay){
+		//this.purchasedStockName.push(sock)
+		portfolio[ticker]['numberOfShares']+=numberShares
+		portfolio[ticker]['totalValue']+= numberShares*pricePerShare
+		portfolio[ticker]['isInPortfolio']=true
+		holding = {
+			day: currentDay,
+			numberOfShares: numberShares, 
+			totalValue: numberShares*pricePerShare,
+			percentageInvested: totalValue/currentMoney * 100
+		}
+		portfolio[ticker]['tradeHistory'].push(holding)
+		currentMoney-= numberShares*pricePerShare
+
+		if (this.holding['percentageInvested']>= 50 ){
+			this.overConfidence+= 10
+		}
 	}
 
 	enableAdvisor(){
 		this.isAdvisorEnabled = true
-		this.initialMoney = this.initialMoney - 1500
+		this.initialMoney = this.initialMoney - 100
+	}
+
+	// this needs to check every day 
+	checkAdvisor(){
+		if (isAdvisorEnabled === false){
+			overConfidence+= 100
+		}
 	}
 
 	updatePortfolioPercentage(){
 		// Do the math to update portfolio percentages
 		//return updated this.endPercent 
-		/*
+		// suggested ideal percent 
 
-		portfolio = {
-			"GOOG":{
-				"numberOfShares":20
-				"totalValue":600
-				"percentageValue": 15
-				"recommendedPercentage":None
-				"tradeHistory":[
-					{
-						"day":
-						"value":
-						"type":
+		portfolio.forEach(stock=> {
+			
+		});
+		
+	} 
 	
-					}
-
-
-				]
-			},
-			"TSLA":{
-	
-			}
-	
-		}
-
-
-
-
-
-
-		*/
-	}
+// This fuction needs to update the updateProtfolioPercentage function
 
 	start(){
 		this.timeUtility.start()
 		
-	}
 
+	
+		
+	}
+	
 	isPortfolioBalanced(){
 		//Do the math to see if portfolio is balanced
 		//return Boolean or Value
@@ -90,10 +137,10 @@ OVER CONFIDENCE METRICS
 
 1. Are the picking an advisor when they start the game
 2. Are the investing large sums of money, early in the game 
+
 3. Is their portfolio extremely unbalanced
 4. Are they reading articles -> how often they pause the game, how long spent in pause state? 
 */
-
 
 
 
