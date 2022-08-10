f564e4aca8c3f24362fab76460dc2b2337c21602
ashirod <arnav.shirodkar@morningstar.com>
2022-08-10 08:20:12 -0500

    Build of Github Pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV==="production"?"Morningstar_CaseStudy":"/",
   css: {
     loaderOptions: {
       sass: {
